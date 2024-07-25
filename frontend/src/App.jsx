eae61b4a3242f7f96e8d50162d32aed978fdb01f
kundan <kundankumar28884@gmail.com>
2024-07-25 13:49:51 +0530

    loader

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,16 @@
-import { useState } from "react";
-import { Button } from "./components/ui/button";
-
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div>
-      <Button>Click me</Button>
+    <div className="h-screen flex items-center justify-center text-2xl text-emerald-600 font-bold">
+      <div className="inline-flex items-end gap-2">
+        <span className="">Under Progress </span>
+        <div class="loading-dots">
+          <div class="dot"></div>
+          <div class="dot"></div>
+          <div class="dot"></div>
+        </div>
+      </div>
     </div>
   );
 }
