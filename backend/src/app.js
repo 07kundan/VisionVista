cad67b3b5f6d9f62844a96f24be0f6476484e03a
kundan <kundankumar28884@gmail.com>
2024-08-11 10:59:57 +0530

    fixed cors error

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,23 +3,8 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 
 const app = express();
-const allowedOrigins = [
-  process.env.CORS_ORIGIN_PRODUCTION,
-  process.env.CORS_ORIGIN_DEV,
-];
 
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true,
-  })
-);
+app.use(cors());
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
